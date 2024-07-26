4694203248547553a8c89b7eb28c9c20ee6f9afa
Agung Sasongko <89963730+agoenxz2186@users.noreply.github.com>
2024-07-26 18:38:30 +0700

    perbaiki run-sh

--- run.sh
+++ run.sh
@@ -2,4 +2,9 @@
 cd /var/www
 php artisan migrate
 php artisan storage:link
+chmod -R 775 /var/www/storage
+chmod -R 775 /var/www/public/storage
+chown -R www-data:www-data /var/www/storage
+chown -R www-data:www-data /var/www/public/storage
+
 supervisord -c /etc/supervisord.conf
