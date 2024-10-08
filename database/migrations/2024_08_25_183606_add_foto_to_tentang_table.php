456e47d81aaa77d35f264a4f10fc95e7a6f3e3bf
12210778-MaulanaAdityaPratama <113579366+12210778-MaulanaAdityaPratama@users.noreply.github.com>
2024-10-08 16:10:47 +0700

    update table migration tentang kami

--- database/migrations/2024_08_25_183606_add_foto_to_tentang_table.php
+++ database/migrations/2024_08_25_183606_add_foto_to_tentang_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('tentang_kami', function (Blueprint $table) {
-            $table->string('foto')->nullable();
+           $table->string('foto')->nullable()->change();
         });
     }
 
